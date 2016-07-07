24165da4e01b9f94e5ec7d5c79968e00157f018d
Jason Mulvenna <jason.mulvenna@gmail.com>
2016-07-07 14:53:37 +1000

    Updated dependiencies

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [clj-tandem "0.1.3"]
+                 [clj-tandem "0.1.5"]
                  [me.raynes/fs "1.4.6"]
                  [org.clojure/tools.cli "0.3.3"]]
   :main ^:skip-aot protein-report.core
